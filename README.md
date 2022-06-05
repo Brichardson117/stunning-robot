b1efeebe3d1ed2171096a88219041619dec6a915
Brittney <brittneyrichardson8@gmail.com>
2022-06-04 19:57:21 -0500

    updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# stunning-robot
+# Tech Blog
+
+  ## License
+  
+  ### This project uses GNU_AGPLv3
+  
+  [GNU_AGPLv3](https://opensource.org/license/GNU_AGPLv3)
+  
+  
+  ![GNU_AGPLv3](https://shields.io/badge/license-GNU_AGPLv3-brightgreen)
+  
+
+  ## Description
+  a webiste where developers can write and comment blog post
+  
+  
+  ## Questions
+ If you have additional questions you can reach me here brittneyrichardson8@gmail.com and https://github.com/Brichardson117
---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helper');
+
+const SequlizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  Store: new SequlizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess))
+ 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
